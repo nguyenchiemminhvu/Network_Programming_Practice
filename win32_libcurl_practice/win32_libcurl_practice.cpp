03392fe89fc6c45101e3a3f947155b445bfd08c5
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-04-16 10:21:50 +0700

    update

--- win32_libcurl_practice/win32_libcurl_practice.cpp
+++ win32_libcurl_practice/win32_libcurl_practice.cpp
@@ -1,5 +1,27 @@
+#define CURL_STATICLIB
+
 #include <curl/curl.h>
 #include <iostream>
+
+#if _WIN64
+    #ifdef _DEBUG
+        #pragma comment(lib, "libcurl/lib_x64_debug/libcurl_a_debug.lib")
+    #else
+        #pragma comment(lib, "libcurl/lib_x64_release/libcurl_a.lib")
+    #endif
+#elif _WIN32
+    #ifdef _DEBUG
+        #pragma comment(lib, "libcurl/lib_x32_debug/libcurl_a_debug.lib")
+    #else
+        #pragma comment(lib, "libcurl/lib_x32_release/libcurl_a.lib")
+    #endif
+#endif
+
+#pragma comment(lib, "Normaliz.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "Wldap32.lib")
+#pragma comment(lib, "Crypt32.lib")
+#pragma comment(lib, "advapi32.lib")
 
 using namespace std;
 
