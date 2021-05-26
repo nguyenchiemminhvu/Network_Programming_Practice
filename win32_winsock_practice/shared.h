e53b12b401c77d8b1bbc14c4244c8bdc6796f731
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-05-26 16:54:56 +0700

    update

--- win32_winsock_practice/shared.h
+++ win32_winsock_practice/shared.h
@@ -2666,13 +2666,50 @@ namespace ClientServer_IOCP_Model
             }
         }
 
-        DWORD buffer_len;
-        WSAOVERLAPPED* overlapped_result;
-        SOCKET_STATE* socket_state_result;
+        DWORD bytes_transferred;
+        WSAOVERLAPPED* overlapped_result = NULL;
+        SOCKET_STATE* socket_state_result = NULL;
         while (true)
         {
-            
+            rc = GetQueuedCompletionStatus(IOCP_object, &bytes_transferred, (ULONG_PTR*)&socket_state_result, &overlapped_result, INFINITE);
+            if (!rc)
+            {
+                WS_ERROR("Get completion port status failed with code:", GetLastError());
+                continue;
+            }
+
+            if (!socket_state_result || !overlapped_result)
+            {
+                WS_LOG("Unknown behaviour");
+                break;
+            }
+
+            switch (socket_state_result->operation_type)
+            {
+            case OP_ACCEPT:
+                WS_LOG("OP_ACCEPT completed");
+                break;
+
+            case OP_READ:
+                WS_LOG("OP_READ completed");
+                break;
+
+            case OP_WRITE:
+                WS_LOG("OP_WRITE completed");
+                break;
+
+            default:
+                WS_LOG("unknown behaviour!!!");
+                closesocket(socket_state_result->socket);
+                SAFE_DELETE(socket_state_result);
+                SAFE_DELETE(overlapped_result);
+                break;
+            }
         }
+
+        closesocket(soc_listen);
+        SAFE_DELETE(socket_state_result);
+        SAFE_DELETE(overlapped_result);
     }
 
     void UDP_Server()
