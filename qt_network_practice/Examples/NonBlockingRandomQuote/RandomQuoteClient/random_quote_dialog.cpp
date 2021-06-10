9e5e2f87d810df5c5bc10a20bcf92a7259bcd9d5
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-06-10 16:35:50 +0700

    update

--- qt_network_practice/Examples/NonBlockingRandomQuote/RandomQuoteClient/random_quote_dialog.cpp
+++ qt_network_practice/Examples/NonBlockingRandomQuote/RandomQuoteClient/random_quote_dialog.cpp
@@ -6,10 +6,28 @@ Random_Quote_Dialog::Random_Quote_Dialog(QWidget *parent)
     , ui(new Ui::Random_Quote_Dialog)
 {
     ui->setupUi(this);
+
+    InitUI();
+    EstablishSignalsAndSlots();
 }
 
 Random_Quote_Dialog::~Random_Quote_Dialog()
 {
     delete ui;
+}
+
+void Random_Quote_Dialog::SendRequest()
+{
+
+}
+
+void Random_Quote_Dialog::InitUI()
+{
+
+}
+
+void Random_Quote_Dialog::EstablishSignalsAndSlots()
+{
+
 }
 
--- qt_network_practice/Examples/NonBlockingRandomQuote/RandomQuoteClient/random_quote_dialog.h
+++ qt_network_practice/Examples/NonBlockingRandomQuote/RandomQuoteClient/random_quote_dialog.h
@@ -2,6 +2,15 @@
 #define RANDOM_QUOTE_DIALOG_H
 
 #include <QDialog>
+#include <QString>
+#include <QThread>
+#include <QTcpSocket>
+#include <QDataStream>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QPlainTextEdit>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Random_Quote_Dialog; }
@@ -15,7 +24,27 @@ public:
     Random_Quote_Dialog(QWidget *parent = nullptr);
     ~Random_Quote_Dialog();
 
+signals:
+
+public slots:
+    void SendRequest();
+
+private:
+    void InitUI();
+    void EstablishSignalsAndSlots();
+
 private:
     Ui::Random_Quote_Dialog *ui;
+
+private:
+    QTcpSocket *m_soc_client;
+
+    QLabel *m_lHost;
+    QLabel *m_lPort;
+    QLineEdit *m_leHost;
+    QLineEdit *m_lePort;
+    QPushButton *m_pbRequest;
+    QPushButton *m_pbQuit;
+    QPlainTextEdit *m_pteQuote;
 };
 #endif // RANDOM_QUOTE_DIALOG_H
