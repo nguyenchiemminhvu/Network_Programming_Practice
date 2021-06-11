035eca1273ff819ffbb342e9dd745a4ef166459b
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-06-11 13:15:44 +0700

    update

--- qt_network_practice/Examples/Broadcast/BroadcastReceiver/broadcastdialog.cpp
+++ qt_network_practice/Examples/Broadcast/BroadcastReceiver/broadcastdialog.cpp
@@ -6,6 +6,10 @@ BroadcastDialog::BroadcastDialog(QWidget *parent)
     , ui(new Ui::BroadcastDialog)
 {
     ui->setupUi(this);
+
+    InitUI();
+    InitSockets();
+    EstablishSignalsAndSlots();
 }
 
 BroadcastDialog::~BroadcastDialog()
@@ -13,3 +17,37 @@ BroadcastDialog::~BroadcastDialog()
     delete ui;
 }
 
+void BroadcastDialog::ProcessPendingDatagrams()
+{
+    QByteArray datagram;
+
+    while (m_broadcast_listener->hasPendingDatagrams())
+    {
+        datagram.resize(m_broadcast_listener->pendingDatagramSize());
+        m_broadcast_listener->readDatagram(datagram.data(), datagram.size());
+        m_lStatus->setText(tr("Received datagram: %1").arg(datagram.constData()));
+    }
+}
+
+void BroadcastDialog::InitUI()
+{
+    m_lStatus = new QLabel(tr("Listening..."));
+    m_pbQuit = new QPushButton(tr("Quit"));
+
+    QGridLayout *main_layout = new QGridLayout();
+    main_layout->addWidget(m_lStatus, 0, 0, 1, 3);
+    main_layout->addWidget(m_pbQuit, 1, 2, 1, 1);
+    this->setLayout(main_layout);
+}
+
+void BroadcastDialog::InitSockets()
+{
+    m_broadcast_listener = new QUdpSocket(this);
+    m_broadcast_listener->bind(45454, QUdpSocket::BindFlag::ShareAddress);
+}
+
+void BroadcastDialog::EstablishSignalsAndSlots()
+{
+    connect(m_pbQuit, &QPushButton::clicked, this, &BroadcastDialog::close);
+    connect(m_broadcast_listener, &QUdpSocket::readyRead, this, &BroadcastDialog::ProcessPendingDatagrams);
+}
--- qt_network_practice/Examples/Broadcast/BroadcastReceiver/broadcastdialog.h
+++ qt_network_practice/Examples/Broadcast/BroadcastReceiver/broadcastdialog.h
@@ -2,6 +2,11 @@
 #define BROADCASTDIALOG_H
 
 #include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QUdpSocket>
+#include <QByteArray>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class BroadcastDialog; }
@@ -15,7 +20,21 @@ public:
     BroadcastDialog(QWidget *parent = nullptr);
     ~BroadcastDialog();
 
+private slots:
+    void ProcessPendingDatagrams();
+
+private:
+    void InitUI();
+    void InitSockets();
+    void EstablishSignalsAndSlots();
+
 private:
     Ui::BroadcastDialog *ui;
+
+private:
+    QUdpSocket *m_broadcast_listener;
+
+    QLabel *m_lStatus;
+    QPushButton *m_pbQuit;
 };
 #endif // BROADCASTDIALOG_H
